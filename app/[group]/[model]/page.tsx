824bc4ae61d9e6eacb598494e9b8ca7db0b5b916
ht.pang <hantian.pang@gmail.com>
2024-05-05 21:11:09 +0800

    adjust ui, add buymeacoffee

--- app/[group]/[model]/page.tsx
+++ app/[group]/[model]/page.tsx
@@ -26,7 +26,7 @@ export default function Page({
 
   return (
     <main className="flex-1 flex flex-col items-center bg-base-200">
-      <h1 className="text-lg font-semibold m-4">
+      <h1 className="text-lg font-semibold bg-secondary text-secondary-content rounded-2xl mt-2 py-2 px-4">
         Count Token for {model.name}
       </h1>
       <Suspense>
--- app/components/tokencounter.tsx
+++ app/components/tokencounter.tsx
@@ -22,7 +22,6 @@ export default function TokenCounter(props: {
     let tokens = 0;
     if (tokenizer.current) {
       let encoding = tokenizer.current.encode(text);
-      console.log(encoding);
       tokens = encoding.length;
     }
     setTokens(tokens);
@@ -30,13 +29,20 @@ export default function TokenCounter(props: {
 
   return (
     <div className="flex-1 w-full lg:max-h-[50vh] lg:max-w-[80vh] flex flex-col items-center px-4">
+      <p className="text-sm bg-accent text-accent-content rounded-lg m-2 py-0.5 px-2">
+        * No need to worry about your data, counter is running on your browser.
+      </p>
       <textarea
         className="textarea textarea-bordered textarea-md rounded-md flex-1 w-full bg-base-100"
         placeholder="Enter your text here..."
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></textarea>
-      <p className="text-base font-bold m-4">Tokens: {tokens}</p>
+      {tokenizer.current ? (
+        <p className="text-base font-bold m-4">Tokens: {tokens}</p>
+      ) : (
+        <span className="loading loading-dots loading-md m-4"></span>
+      )}
     </div>
   );
 }
