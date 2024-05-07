9b93611b8a013a934d966a827d785af232e29275
ppaanngggg <hantian.pang@gmail.com>
2024-05-07 20:17:20 +0800

    add google analytics

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,22 +1,26 @@
+import { Suspense } from "react";
+
 export default function Footer() {
   return (
     <footer className="footer footer-center p-4 bg-neutral text-neutral-content h-12">
       <aside>
         <p>Copyright © 2024 - All right reserved</p>
       </aside>
-      <script
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        data-id="ppaanngggg"
-        data-description="Support me on Buy me a coffee!"
-        data-message=""
-        data-color="#FF813F"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-        defer
-      ></script>
+      <Suspense>
+        <script
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          data-id="ppaanngggg"
+          data-description="Support me on Buy me a coffee!"
+          data-message=""
+          data-color="#FF813F"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+          defer
+        ></script>
+      </Suspense>
     </footer>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/app/components/navbar";
 import { buildMetadata } from "@/app/utils/metadata";
 import Footer from "@/app/components/footer";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <GoogleAnalytics gaId="G-XTJZTH352K" />
         <Analytics />
       </body>
     </html>
