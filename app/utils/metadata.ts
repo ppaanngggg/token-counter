9fe1e15338e9116c8d8cdb978b964761f244a9cd
ppaanngggg <hantian.pang@gmail.com>
2024-05-07 22:45:15 +0800

    add yandex verification
    fix yandex

--- app/utils/metadata.ts
+++ app/utils/metadata.ts
@@ -4,7 +4,10 @@ export function buildMetadata(title: string, description: string): Metadata {
   return {
     title: title,
     description: description,
-    verification: { google: "1IppxSZ3nTfcrEZ--z1XPzzH5Km-0rmvAaKZ4eBs38U" },
+    verification: {
+      google: "1IppxSZ3nTfcrEZ--z1XPzzH5Km-0rmvAaKZ4eBs38U",
+      yandex: "e471e35b09a83570",
+    },
     metadataBase: new URL("https://token-counter.app"),
     alternates: {
       canonical: "/",
