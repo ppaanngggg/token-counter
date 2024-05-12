19fed3210212b30b178f32bc6949f9d858b7db5c
ppaanngggg <hantian.pang@gmail.com>
2024-05-12 15:42:00 +0800

    adjust faq, add more icon to navbar

--- app/components/faq.tsx
+++ app/components/faq.tsx
@@ -1,52 +1,56 @@
 import Link from "next/link";
 
+function LinkOfXenovaTransformers() {
+  return (
+    <Link
+      href="https://xenova.github.io/transformers.js"
+      rel="noopener ugc nofollow"
+      target="_blank"
+      className="underline"
+    >
+      Transformers.js
+    </Link>
+  );
+}
+
 export default function FAQ() {
   const qClass = "text-l font-semibold my-1";
   const aClass = "text-s text-justify my-1";
   return (
-    <div className="card w-full md:w-1/2 bg-accent text-accent-content shadow-xl flex flex-col items-left p-4 mx-auto">
+    <div className="card max-w-[64rem] bg-accent text-accent-content shadow-xl flex flex-col items-left p-4 mx-auto">
       <h2 className="text-xl font-semibold mt-2">FAQ:</h2>
       <div className="divider"></div>
       <h3 className={qClass}>• What is LLM Token Counter?</h3>
       <p className={aClass}>
-        LLM Token Counter is a tool designed to count tokens for various popular
-        LLMs (Language Model Models) such as GPT-3.5, GPT-4, Claude-3, Llama-3,
-        and others.
-      </p>
-      <h3 className={qClass}>• How does the LLM Token Counter work?</h3>
-      <p className={aClass}>
-        LLM Token Counter works by{" "}
-        <Link
-          href="https://github.com/xenova/transformers.js"
-          rel="noopener ugc nofollow"
-          target="_blank"
-          className="underline"
-        >
-          transformers.js
+        LLM Token Counter is a sophisticated tool meticulously crafted to assist
+        users in effectively managing token limits for a diverse array of
+        widely-adopted Language Models (LLMs), including GPT-3.5, GPT-4,
+        Claude-3, Llama-3, and many others. I am committed to continuously
+        expanding the supported models and enhancing the tool&apos;s
+        capabilities to empower you with an optimal experience in leveraging
+        generative AI technology. Should you require any assistance or have
+        suggestions for additional features, please feel free to reach out to me
+        via{" "}
+        <Link href="mailto:hantian.pang@gmail.com" className="underline">
+          email
         </Link>
-        , a JavaScript implementation of the Hugging Face Transformers library.
-        Tokenizers are loaded in your browser and the token count is calculated
-        client-side.
+        .
       </p>
       <h3 className={qClass}>• Why use an LLM Token Counter?</h3>
       <p className={aClass}>
-        Using an LLM Token Counter provides valuable insights into token usage
-        for different LLMs. It enables users to optimize their text inputs and
-        better understand token limitations for specific language model models.
+        Due to the inherent limitations of LLMs, it is crucial to ensure that
+        the token count of your prompt falls within the specified token limit.
+        Exceeding this limit may result in unexpected or undesirable outputs
+        from the LLM.
       </p>
-      <h3 className={qClass}>• Is the LLM Token Counter accurate?</h3>
+      <h3 className={qClass}>• How does the LLM Token Counter work?</h3>
       <p className={aClass}>
-        LLM Token Counter use{" "}
-        <Link
-          href="https://github.com/xenova/transformers.js"
-          rel="noopener ugc nofollow"
-          target="_blank"
-          className="underline"
-        >
-          transformers.js
-        </Link>{" "}
-        to drive the real tokenizer in the browser. That ensures the exact same
-        tokenization as the real model.
+        LLM Token Counter works by utilizing <LinkOfXenovaTransformers />, a
+        JavaScript implementation of the renowned Hugging Face Transformers
+        library. Tokenizers are loaded directly in your browser, enabling the
+        token count calculation to be performed client-side. Thanks to the
+        efficient Rust implementation of the Transformers library, the token
+        count calculation is remarkably fast.
       </p>
     </div>
   );
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { groups } from "@/app/utils/groups";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
@@ -85,24 +86,41 @@ export default function Navbar() {
           })}
         </ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end pr-2 space-x-1">
+        <Link href="mailto:hantian.pang@gmail.com">
+          <Image
+            src="/static/email.svg"
+            alt="emailme"
+            width="36"
+            height="36"
+            className="pr-2"
+          />
+        </Link>
         <Link
-          href={"https://github.com/ppaanngggg/token-counter"}
+          href="https://github.com/ppaanngggg/token-counter"
           rel="noopener ugc nofollow"
           target="_blank"
-          className="px-2"
         >
-          <svg
-            aria-hidden="true"
-            height="24"
-            viewBox="0 0 16 16"
-            version="1.1"
-            width="24"
-            data-view-component="true"
-            className="octicon octicon-mark-github"
-          >
-            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-          </svg>
+          <Image src="/static/github.svg" alt="github" width="24" height="24" />
+        </Link>
+        <Link
+          href="https://buymeacoffee.com/ppaanngggg"
+          rel="noopener ugc nofollow"
+          target="_blank"
+        >
+          <Image
+            src="/static/bmc.svg"
+            alt="buymeacoffee"
+            width="40"
+            height="40"
+          />
+        </Link>
+        <Link
+          href="https://ko-fi.com/ppaanngggg"
+          rel="noopener ugc nofollow"
+          target="_blank"
+        >
+          <Image src="/static/ko-fi.svg" alt="ko-fi" width="30" height="30" />
         </Link>
       </div>
     </header>
