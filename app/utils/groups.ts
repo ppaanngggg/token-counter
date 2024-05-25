a2f27b701135f03933b367cdee078a05e87a8378
ppaanngggg <hantian.pang@gmail.com>
2024-05-25 14:17:48 +0800

    wip

--- app/utils/groups.ts
+++ app/utils/groups.ts
@@ -1,6 +1,7 @@
 interface Model {
   name: string;
   value: string;
+  context: number;
   hub: string;
 }
 
@@ -13,27 +14,41 @@ const groups: Group[] = [
   {
     name: "OpenAI",
     models: [
-      { name: "GPT-4o", value: "gpt-4o", hub: "Xenova/gpt-4o" },
-      { name: "GPT-4 Turbo", value: "gpt-4-turbo", hub: "Xenova/gpt-4" },
-      { name: "GPT-4", value: "gpt-4", hub: "Xenova/gpt-4" },
+      {
+        name: "GPT-4o",
+        value: "gpt-4o",
+        context: 128000,
+        hub: "Xenova/gpt-4o",
+      },
+      {
+        name: "GPT-4 Turbo",
+        value: "gpt-4-turbo",
+        context: 128000,
+        hub: "Xenova/gpt-4",
+      },
+      { name: "GPT-4", value: "gpt-4", context: 8192, hub: "Xenova/gpt-4" },
       {
         name: "GPT-3.5 Turbo",
         value: "gpt-3.5-turbo",
+        context: 16385,
         hub: "Xenova/gpt-3.5-turbo",
       },
       {
         name: "Embedding V3 large",
         value: "text-embedding-3-large",
+        context: 8191,
         hub: "Xenova/text-embedding-ada-002",
       },
       {
         name: "Embedding V3 small",
         value: "text-embedding-3-small",
+        context: 8191,
         hub: "Xenova/text-embedding-ada-002",
       },
       {
         name: "Embedding Ada 002",
         value: "text-embedding-ada-002",
+        context: 8191,
         hub: "Xenova/text-embedding-ada-002",
       },
     ],
@@ -44,31 +59,37 @@ const groups: Group[] = [
       {
         name: "Claude 3 Opus",
         value: "claude-3-opus",
+        context: 200000,
         hub: "Xenova/claude-tokenizer",
       },
       {
         name: "Claude 3 Sonnet",
         value: "claude-3-sonnet",
+        context: 200000,
         hub: "Xenova/claude-tokenizer",
       },
       {
         name: "Claude 3 Haiku",
         value: "claude-3-haiku",
+        context: 200000,
         hub: "Xenova/claude-tokenizer",
       },
       {
         name: "Claude 2.1",
         value: "claude-2.1",
+        context: 200000,
         hub: "Xenova/claude-tokenizer",
       },
       {
         name: "Claude 2.0",
         value: "claude-2.0",
+        context: 100000,
         hub: "Xenova/claude-tokenizer",
       },
       {
         name: "Claude Instant 1.2",
         value: "claude-instant-1.2",
+        context: 100000,
         hub: "Xenova/claude-tokenizer",
       },
     ],
@@ -76,11 +97,22 @@ const groups: Group[] = [
   {
     name: "Meta",
     models: [
-      { name: "Llama 3", value: "llama-3", hub: "Xenova/llama-3-tokenizer" },
-      { name: "Llama 2", value: "llama-2", hub: "Xenova/llama2-tokenizer" },
+      {
+        name: "Llama 3",
+        value: "llama-3",
+        context: 8192,
+        hub: "Xenova/llama-3-tokenizer",
+      },
+      {
+        name: "Llama 2",
+        value: "llama-2",
+        context: 4096,
+        hub: "Xenova/llama2-tokenizer",
+      },
       {
         name: "Code Llama",
         value: "code-llama",
+        context: 16384,
         hub: "Xenova/llama-code-tokenizer",
       },
     ],
