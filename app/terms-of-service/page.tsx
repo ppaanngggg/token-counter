131291f6d31b34d412afa350b32cda39957ef5ae
ht.pang <hantian.pang@gmail.com>
2024-08-04 21:42:58 +0800

    add privacy-policy and tos

---
+++ app/terms-of-service/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <iframe
+      src="https://app.enzuzo.com/policies/tos/81e01d4e-46a0-11ef-b5b1-8f2ce8169a09"
+      className="w-full flex-1"
+    />
+  );
+}
